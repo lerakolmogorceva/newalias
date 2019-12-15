a7bdb536426c2de8e9f1cbd6c82480aad4d58c9e
Валерия Колмогорцева <vrkolmogortseva_1@edu.hse.ru>
2019-12-15 20:45:18 +0300

    Add project files.

---
+++ TeamProjectNew/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.
+        }
+    }
+}
